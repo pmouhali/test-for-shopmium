5a35e4f308489c8950909974ced582719ed309e0
pmouhali <p.m.272019@gmail.com>
2019-01-18 16:11:38 +0100

    create form + validation model

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,9 +4,23 @@ class ShopsController < ApplicationController
   end
 
   def new
+    @shop = Shop.new
   end
 
   def create
+    @shop = Shop.new
+    @shop.chain = "shopmium"
+    @shop.name = params["shop"]["name"]
+    @shop.address = params["shop"]["address"]
+    @shop.city = params["shop"]["city"]
+    @shop.phone = params["shop"]["phone"]
+    @shop.zip = params["shop"]["zip"]
+    @shop.country_code = params["shop"]["country_code"]
+    @shop.latitude = params["shop"]["latitude"]
+    @shop.longitude = params["shop"]["longitude"]
+    @shop.save
+    #flash[:success] = "Shop succesfully added !"
+    redirect_to root_path
   end
 
   def edit
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,17 @@
 class Shop < ApplicationRecord
+	validates :name, length: { minimum: 8 }, presence: true
+	validates :address, length: { minimum: 5 }, presence: true
+	
+	validates :latitude, length: { minimum: 4 }, 
+	presence: true, format: { with: /\A\-?\d+\.\d+\z/,
+    message: "only allows float" }
+
+   	validates :longitude, length: { minimum: 4 }, 
+   	presence: true, format: { with: /\A\-?\d+\.\d+\z/,
+       message: "only allows float" }
+
+    validates :city, presence: true
+    validates :zip, presence: true, length: { is: 15}, numericality: { only_integer: true }
+    validates :phone, presence: true, length: { is: 10}, numericality: { only_integer: true }
+    validates :country_code, presence: true
 end
